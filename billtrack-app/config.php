48d2a3e79e675b8d3a2a57dda7a3c93d467b44ee
Eoban Binder <eoban@eoban.com>
2017-02-13 12:21:43 -0500

    overall for separate config
    all relevant config is now in config.php

---
+++ billtrack-app/config.php
@@ -0,0 +1,27 @@
+<?php
+date_default_timezone_set('America/New_York');
+$title = "Bill Track - Indiana Public Media";
+$htmltitle = "<span>StateImpact Indiana</span> Bill Track";
+$billtrackpath = "//indianapublicmedia.org/ipbs/billtrack/"; // attr for script element
+$billspath = "https://indianapublicmedia.org/ipbs/bills/"; // arg for file_get_contents()
+$org = "Indiana Public Media";
+
+$topics_array[] = "Academics";
+$topics_array[] = "Pre-K";
+$topics_array[] = "State Superintendent";
+$topics_array[] = "Teacher Retirement";
+$topics_array[] = "School Funding";
+$topics_array[] = "Higher Education";
+$topics_array[] = "Local School Boards";
+$topics_array[] = "Health & Safety";
+$topics_array[] = "Student Affairs";
+$topics_array[] = "Teacher Pay";
+$topics_array[] = "Discipline";
+$topics_array[] = "Vouchers";
+$topics_array[] = "Teacher Evaluations";
+$topics_array[] = "Misc.";
+$topics_array[] = "Accountability & Testing";
+$topics_array[] = "Transportation";
+$topics_array[] = "Charter Schools";
+
+?>
--- billtrack-app/index.php
+++ billtrack-app/index.php
@@ -1,58 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
+<?php include('config.php'); ?>
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,700,700italic,900,900italic,400italic" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet" type="text/css">	
-	<link rel="stylesheet" type='text/css' href= "reset.css" />
-	<link rel="stylesheet" type='text/css' href= "style-v4.css" />
+	<link rel="stylesheet" type='text/css' href="reset.css" />
+	<link rel="stylesheet" type='text/css' href="style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.4.1/list.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/list.pagination.js/0.1.1/list.pagination.min.js"></script>
-	<!--list init, edit this-->
-	<script src="/ipbs/billtrack/"></script>
+	<!--list init-->
+	<script src="<?php echo $billtrackpath; ?>"></script>
 	<!--the rest of the js-->
-	<script src="sessiontoolv4.js"></script>
+	<script src="sessiontool.js"></script>
 	
-	<title>Bill Track - Indiana Public Media</title>
+	<title><?php echo $title; ?></title>
 </head>
 
 <body>
 <div id="bills">
 <div id="updatelist">
-<header>
-<h1><span>StateImpact Indiana</span> Bill Track</h1>
-
-<div id="the-selects">
-<!--Dropdown menu for topics-->
-	<select id="select1" class="search">
-    	<option value=" ">Select Topic ↓</option>
-    	<option value="Pre-K">Pre-K</option>
-    	<option value="Teacher-Pay">Teacher Pay</option>
-    	<option value="Vouchers">School vouchers</option>
-	</select>
-<!--Dropdown menu for bills-->	
-	<select id="select2" class="search" >
-	<?php echo file_get_contents('https://indianapublicmedia.org/ipbs/bills/'); // edit this ?>
-	</select>
+	<header>
+	<h1><?php echo $htmltitle; ?></h1>
+	<div id="the-selects">
+	<!--Dropdown menu for topics-->
+		<select id="select1" class="search">
+    		<option value=" ">Select Topic ↓</option>
+    		<?php for($i=0;$i<count($topics_array);$i++) { echo '<option value="' . $topics_array[$i] . '">' . $topics_array[$i] .'</option>';} ?>
+		</select>
+	<!--Dropdown menu for bills-->	
+		<select id="select2" class="search" >
+		<?php echo file_get_contents($bills_path); ?>
+		</select>
+	</div>
+	<div id="search-sort">
+		<!--Search box-->	
+		<input type="text" class="search" placeholder="Search" />
+		<button class="clear-search">Clear</button> 
+		<!--Sort by date button-->
+		<button class="sort" data-sort="date">Sort by date</button> 
+	</div>
+	</header>
+	<ul class="list">
+	</ul>
 </div>
-
-<div id="search-sort">
-<!--Search box-->	
-<input type="text" class="search" placeholder="Search" />
-<button class="clear-search">Clear</button> 
-<!--Sort by date button-->
-<button class="sort" data-sort="date">Sort by date</button> 
+<p id="version">&copy; <?php echo date('Y'); ?> <?php echo $org; ?> &bull; version 0.5</p>
 </div>
-
-</header>
-
-<!--The list-->
-      <ul class="list">
-      </ul>
-</div>
-<p id="version">&copy; <?php date_default_timezone_set('America/New_York'); echo date('Y'); ?> Indiana Public Media &bull; version 0.4</p>
-
 </body>
 </html>
